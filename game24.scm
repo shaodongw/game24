c73e5cb91968151298c9057efee252300bb0b6fd
wsd <wsd@xcn-HP.(none)>
2012-01-04 22:37:00 +0800

    test fail

--- game24.scm
+++ game24.scm
@@ -1,4 +1,4 @@
-;; The first three lines of this file were inserted by DrScheme. They record metadata
+;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname game24) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (define (is24? number)
@@ -9,11 +9,13 @@
 
 (define (have-a-sum-is-24? number lon)
   (cond
-    [(empty lon) false]
+    [(empty? lon) false]
     [else (cond
-            [(empty (rest lon)) false]
+            [(empty? (rest lon)) false]
             [else (cond
                     [(is24? (+ number (first lon) )) true]
                     [else (have-a-sum-is-24? number (rest lon))])])]))
                    
-          
+(define test-1 (have-a-sum-is-24? 20 (list 2 3)))
+
+(define test-2 (have-a-sum-is-24? 20 (list 2 3 4)))
