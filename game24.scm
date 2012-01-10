e4f24720ca9b9cc0111e49d72659ab524b997202
Wang Shaodong <shaodong@njit.edu.cn>
2012-01-10 09:38:29 +0800

    go on to improve it

--- game24.scm
+++ game24.scm
@@ -1,19 +1,21 @@
-;; The first three lines of this file were inserted by DrScheme. They record metadata
+;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname game24) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname game24) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (define (each-other lon)
   (cond
     [(empty? lon) empty]
     [else (one-others (first lon) (rest lon))]))
 
 (define (one-others number lon)
-  (cond
-    [(empty? lon) number]
-    [else (cons (+ number (first lon)) (rest lon))]))
+(local(
+  (define (one-others-local number lon lon-leader)
+    (cond
+      [(empty? lon) empty]
+      [else (append lon-leader (cons (+ number (first lon)) (one-others number (rest lon))))])))
+  (one-others-local number lon empty)))
 
-
-(define test1 (each-other (list 2 8 5 3)))
-(define test2 (each-other (list 3 3 20 4)))
+(define test1 (each-other (list 2 8 5)))
+(define test2 (each-other (list 2 8)))
 (define test3 (each-other (list 79)))
 (define test4 (each-other empty))
 
