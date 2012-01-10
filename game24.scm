90ba69d08c639125cf0dae8288b76c070b15d943
Wang Shaodong <shaodongw@gmail.com>
2012-01-10 17:04:27 +0800

    still have a bug: result missing the skipped numbers in a list

--- game24.scm
+++ game24.scm
@@ -1,27 +1,36 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; The first three lines of this file were inserted by DrScheme. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname game24) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (define (each-other lon)
   (cond
     [(empty? lon) empty]
-    [else (one-others lon)]))
+    [else
+     (append (first-others lon) 
+             (first-others (rest lon)))]))
 
-(define (one-others lon)
+(define (one-others number lon)
   (local(
-         (define (one-others-local lon lon-leader)
+         (define (one-others-local number lon lon-leader)
            (cond
              [(empty? lon) empty]
-             [(empty? (rest lon)) empty]
              [else
-              (cons (append lon-leader 
-                            (cons (+ (first lon) (first (rest lon)))
-                                  (rest (rest lon))))
-                    (one-others-local (rest lon) (cons (first lon) lon-leader)))])))
-    (one-others-local lon empty)))
+              (cons
+               (append lon-leader 
+                       (cons (+ number (first lon)) (rest lon)))
+               (one-others-local number (rest lon) (append lon-leader (cons (first lon) empty))))])))
+    (one-others-local number lon empty)))
+
+(define (first-others lon)
+  (cond
+    [(empty? lon) empty]
+    [else
+     (one-others (first lon) (rest lon))]))
+
 
 (define test5 (each-other (list 2 8 5 9)))
 (define test1 (each-other (list 2 8 5)))
 (define test2 (each-other (list 2 8)))
 (define test3 (each-other (list 79)))
 (define test4 (each-other empty))
+
 
