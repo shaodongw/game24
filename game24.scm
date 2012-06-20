8d4d10f8711cb120c7422345a555a9f3c0fc224e
Wang <shaodong@chen>
2012-06-20 09:35:49 +0800

    debug on first-orhers0

--- game24.scm
+++ game24.scm
@@ -1,4 +1,4 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; The first three lines of this file were inserted by DrScheme. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname game24) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 (define final 24)
@@ -28,8 +28,8 @@
   (cond
     [(empty? lop) empty]
     [else
-      (cons
-        (poker-number (first lop)) (unpack-num (rest lop)))]))
+     (cons
+      (poker-number (first lop)) (unpack-num (rest lop)))]))
 
 (define (resolution-one lopoker) 
   (cond
@@ -72,9 +72,9 @@
     [(empty? (rest lop)) empty]
     [(empty? (rest (rest lop))) (list (append leader (list (op-merge-plus (first lop) (first (rest lop))))))]
     [else
-      (append 
-        (append leader (list (op-merge-plus (first lop) (first (rest lop)))) (rest (rest lop)))
-        (first-others0 (rest lop) (append leader (list (first lop)))))])) 
+     (append 
+      (append leader (list (op-merge-plus (first lop) (first (rest lop)))) (rest (rest lop)))
+      (first-others0 (rest lop) (append leader (list (first lop)))))])) 
 
 (define (one-others p lop leader)
   (cond
@@ -88,72 +88,13 @@
                (rest lop)))
       (one-others p (rest lop) (append leader (list (first lop)))))]))
 
-(define testx (first-others0 (pack (list 1 2 3 4)) empty ))
+(define testx (first-others0 (pack (list 1 2)) empty ))
 
 (define test1 (game24 (list 2 3 10 9)))
 (define test2 (game24 (list 2 3 10 8)))
 (define test3 (game24 (list )))
 
-;; Data Definition
-(define-struct node (lon path))
-;; a node: (make-node lon path)
-;; where lon is a list of numbers, path is a series of number and operation symbol
 
-;(define (go lnode)
-;  (cond
-;    [(is-leaf? (first lnode)) lnode]
-;    [(empty? (first lnode)) lnode]
-;    [else (go lnode)]))
-
-;; solve: list of node -> list of node
-;; At first the input list of node only has one node - root node.
-;; Each time solve it, the number list in the node will be reduced,
-;;   and the nodes in list are increased,
-;;   until the node is reduced to leaf node.
-;; The result is the list of node which match 24.
-
-;(define (solve node)
-;  (cond
-;    [(cons? (reduce node))
-;     (cons (reduce (first (reduce node))) 
-;     (append (reduce (first lnode)) (solve (rest lnode)))]
-;    [else empty]))
-
-(define (l-reduce lnode)
-  (cond
-    [(empty? lnode) empty]
-    [else
-     (cond
-       [(cons? (reduce (first lnode))) (l-reduce (append (reduce (first lnode)) (l-reduce (rest lnode))))]
-       [else (cons (reduce (first lnode)) (l-reduce (rest lnode)))])]))
-
-(define (is-solu? node)
-  (and
-   (is-leaf? node)
-   (= 24 (first (node-lon node)))))
-
-(define (is-leaf? node)
-  (empty? (rest (node-lon node))))
-
-
-
-(define-struct merge (result expression))
-
-(define (operate op number1 number2)
-  (make-merge (op number1 number2) (list op number1 number2)))
-
-(define (four-op nu1 nu2)
-  (list
-   (operate + nu1 nu2)
-   (operate - nu1 nu2)
-   (operate - nu2 nu1)
-   (operate * nu1 nu2)
-   (operate / nu1 nu2)
-   (operate / nu2 nu1)))
-
-
-;(define test (l-reduce (list (make-node (list 5 3 9) (list '+ 88 99)) (make-node (list 4 1 6) (list '+ 66 77)) )))
-;(define test (l-reduce (list (make-node (list 5 3 4 9) (list '+ 88 99)) )))
 
 
 ;(display test1)
@@ -166,11 +107,11 @@
   (cond
     [(empty? lolop) empty]
     [else
-      (
+     (
       (display (first lolop))
       (newline)
       (show (rest lolop)))]))
 
-(testx)
-;(show testx)
+; testx
+(show testx)
 ;(op-merge-plus (make-poker 6 6) (make-poker 7 7))
