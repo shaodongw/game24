2cf39e7c6e9e10e9a542b48c4819ccaca0b1bf05
Wang Shaodong <wsd@xcn-HP>
2012-06-21 21:02:51 +0800

    The program works.

--- game24.scm
+++ game24.scm
@@ -1,4 +1,4 @@
-;; The first three lines of this file were inserted by DrScheme. They record metadata
+;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname game24) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 (define final 24)
@@ -41,8 +41,8 @@
      ; only one poker(combined) in the list but is not the final number
      empty]
     [else
-      ; there are more poker(combined) in the list, so merge one and the others...
-      (resolution-all (each-other lop))]))
+     ; there are more poker(combined) in the list, so merge one and the others...
+     (resolution-all (each-other lop))]))
 
 
 ; (define (reduce lop)
@@ -56,22 +56,22 @@
 
 (define (op-merge-plus p1 p2)
   (make-poker (+ (poker-number p1) (poker-number p2))
-              (list '+ (poker-path p1) (poker-path p2))))
+              (list (poker-path p1) '+ (poker-path p2))))
 (define (op-merge-mul p1 p2)
   (make-poker (* (poker-number p1) (poker-number p2))
-              (list '* (poker-path p1) (poker-path p2))))
+              (list (poker-path p1) '* (poker-path p2))))
 (define (op-merge-sub p1 p2)
   (make-poker (- (poker-number p1) (poker-number p2))
-              (list '- (poker-path p1) (poker-path p2))))
+              (list (poker-path p1) '- (poker-path p2))))
 (define (op-merge-rev-sub p1 p2)
   (make-poker (- (poker-number p2) (poker-number p1))
-              (list '- (poker-path p2) (poker-path p1))))
+              (list (poker-path p2) '- (poker-path p1))))
 (define (op-merge-div p1 p2)
   (make-poker (/ (poker-number p1) (poker-number p2))
-              (list '/ (poker-path p1) (poker-path p2))))
+              (list (poker-path p1) '/ (poker-path p2))))
 (define (op-merge-rev-div p1 p2)
   (make-poker (/ (poker-number p2) (poker-number p1))
-              (list '- (poker-path p2) (poker-path p1))))
+              (list (poker-path p2) '/ (poker-path p1))))
 
 (define (each-other lop)
   (local(
@@ -111,35 +111,41 @@
              [(empty? lop) empty]
              [else
               (append
-              (list
-               (append leader
-                       (list (op-merge-plus p (first lop)))
-                       (rest lop)))
-              (list
-               (append leader
-                       (list (op-merge-mul p (first lop)))
-                       (rest lop)))
-              (list
-               (append leader
-                       (list (op-merge-sub p (first lop)))
-                       (rest lop)))
-              (list
-               (append leader
-                       (list (op-merge-rev-sub p (first lop)))
-                       (rest lop)))
-;              (list
-;               (append leader
-;                       (list (op-merge-div p (first lop)))
-;                       (rest lop)))
-;              (list
-;               (append leader
-;                       (list (op-merge-rev-div p (first lop)))
-;                       (rest lop)))
-              
+               (list
+                (append leader
+                        (list (op-merge-plus p (first lop)))
+                        (rest lop)))
+               (list
+                (append leader
+                        (list (op-merge-mul p (first lop)))
+                        (rest lop)))
+               (list
+                (append leader
+                        (list (op-merge-sub p (first lop)))
+                        (rest lop)))
+               (list
+                (append leader
+                        (list (op-merge-rev-sub p (first lop)))
+                        (rest lop)))
+               (cond
+                 [(= 0 (poker-number (first lop))) empty]
+                 [else
+                    (list
+                        (append leader
+                                (list (op-merge-div p (first lop)))
+                                (rest lop)))])
+               (cond
+                 [(= 0 (poker-number p)) empty]
+                 [else
+                    (list
+                        (append leader
+                                (list (op-merge-rev-div p (first lop)))
+                                (rest lop)))])
+               
                (one-to-list0 p (rest lop) (append leader (list (first lop)))))])))
     (one-to-list0 p lop empty)))
 
-(define testx (game24 (list 1 2 3 4)))
+(define testx (game24 (list 1 5 5 5)))
 ;(define testx (resolution-all (list (pack (list 1 2 3 4)))))
 ;(define testx (resolution-one (list (make-poker 25 25) )))
 ;(define testx (resolution-one (list (make-poker 10 10) (make-poker 14 14))))
